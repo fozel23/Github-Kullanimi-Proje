11a01813a2957de28e8b10d2726a22f1ed6ee7ed
fozel23 <fgoa23@gmail.com>
2024-11-19 11:08:29 +0300

    ilk değişiklik

--- src/kisiselKullanim/gitHuBKullanimi1.java
+++ src/kisiselKullanim/gitHuBKullanimi1.java
@@ -3,5 +3,6 @@ package kisiselKullanim;
 public class gitHuBKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Thats it");
     }
 }
