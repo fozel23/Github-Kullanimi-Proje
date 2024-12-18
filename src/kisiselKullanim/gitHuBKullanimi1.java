d34230fba6158cff6371ab9fde04c4e4f6a9fdfd
fozel23 <fgoa23@gmail.com>
2024-12-18 18:48:13 +0300

    degisti2

--- src/kisiselKullanim/gitHuBKullanimi1.java
+++ src/kisiselKullanim/gitHuBKullanimi1.java
@@ -4,6 +4,8 @@ public class gitHuBKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Thats it");
+
+        System.out.println( "hasan geldi");
     }
 }
 
--- src/kisiselKullanim/ozellik1.java
+++ src/kisiselKullanim/ozellik1.java
@@ -3,5 +3,10 @@ package kisiselKullanim;
 public class ozellik1 {
     public static void main(String[] args) {
         System.out.println("feyzullah branch'inde calisiyorum");
+
+
+        System.out.println(("osman gelmedi1"));
+        System.out.println("hösin baba");
+
     }
 }
